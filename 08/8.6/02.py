a9847702a55b747b830d0bb546b230d52e1671e9
justP-official <red-hot-shot@yandex.ru>
2023-11-19 12:38:14 +0300

    8.6 Обработка исключения FileNotFoundError и менеджер контекста

---
+++ 08/8.6/02.py
@@ -0,0 +1,12 @@
+"""
+Подвиг 2. Имеется фрагмент программы.
+При его выполнении возникает ошибка FileNotFoundError.
+Запишите конструкцию try / except, чтобы отображалось сообщение "File Not Found", если файл не удается открыть.
+"""
+
+try:
+    f = open("abc.txt")
+    r = f.read(1)
+    f.close()
+except FileNotFoundError:
+    print("File Not Found")
