6936eef402eaaf63e8075aef3bc8064c7d2d8be0
justP-official <red-hot-shot@yandex.ru>
2023-11-20 13:30:18 +0300

    9.1 Выражения-генераторы

---
+++ 09/9.1/07.py
@@ -0,0 +1,20 @@
+"""
+Подвиг 7. Используя символы малых букв латинского алфавита (строка ascii_lowercase):
+
+from string import ascii_lowercase
+
+запишите генератор, который бы возвращал все сочетания из двух букв латинского алфавита.
+Выведите первые 50 сочетаний на экран в строку через пробел.
+
+Например, первые семь начальных сочетаний имеют вид:
+
+aa ab ac ad ae af ag
+"""
+
+from string import ascii_lowercase
+
+string_gen = (i + j for i in ascii_lowercase
+              for j in ascii_lowercase)
+
+for _ in range(50):
+    print(next(string_gen), end=' ')
